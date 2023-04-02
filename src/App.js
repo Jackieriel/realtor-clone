ded9e3f17a6c2b65e32f63985a0a9f77c7157a0d
jackieriel <jonahjacksonj@gmail.com>
2023-04-02 15:29:20 +0100

    Implemented header component navigation

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-   <Router>
-    <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/profile" element={<Profile></Profile>}></Route>
-      <Route path="/sign-in" element={<Signin></Signin>}></Route>
-      <Route path="/sign-up" element={<Signup></Signup>}></Route>
-      <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>}></Route>
-      <Route path="/offers" element={<Offers></Offers>}></Route>
-    </Routes>
-   </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route path="/sign-in" element={<Signin></Signin>}></Route>
+        <Route path="/sign-up" element={<Signup></Signup>}></Route>
+        <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>}></Route>
+        <Route path="/offers" element={<Offers></Offers>}></Route>
+      </Routes>
+    </Router>
   )
 }
