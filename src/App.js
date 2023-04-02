c6b6246ecb32efb41c888b7ce42067bb79198284
jackieriel <jonahjacksonj@gmail.com>
2023-04-02 14:02:17 +0100

    Install Router and create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-center">
-      Hello world!
-    </h1>
+   <Router>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/profile" element={<Profile></Profile>}></Route>
+      <Route path="/sign-in" element={<Signin></Signin>}></Route>
+      <Route path="/sign-up" element={<Signup></Signup>}></Route>
+      <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>}></Route>
+      <Route path="/offers" element={<Offers></Offers>}></Route>
+    </Routes>
+   </Router>
   )
 }
